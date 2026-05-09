d4ccfcde34cce149874b29917bde5cd051988e82
test-gugan <thalapathigugan.k@senatio.com>
2026-05-09 23:34:54 +0530

    fix the mobile responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${fraunces.variable} ${geist.variable} ${geistMono.variable}`}
     >
       <head>
